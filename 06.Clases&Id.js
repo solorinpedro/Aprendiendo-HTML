22ab051c85bd384f22301edbf75e66494232da08
Pedro Luis Solorin <89814239+solorinpedro@users.noreply.github.com>
2021-12-21 17:46:08 -0400

    Add files via upload

---
+++ 06.Clases&Id.js
@@ -0,0 +1,7 @@
+const box=document.querySelector('.box')
+
+box.style.width="100px"
+box.style.height="100px"
+box.style.border="1px solid black"
+box.style.backgroundColor="red"
+box.style.color="white"
